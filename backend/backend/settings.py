e56d65e2d73f98e76c64b156938fbc19cbe3bc9d
Theycallmeki <laxamana.kian13@gmail.com>
2024-03-09 23:38:38 +0800

    dsd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -189,8 +189,8 @@ SIMPLE_JWT = {
 
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'akosivalorant@gmail.com'  
+EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
-EMAIL_HOST_USER = " "
+EMAIL_HOST_USER = "akosivalorant@gmail.com "
 EMAIL_HOST_PASSWORD = "cvoz mnxh qtvw rdbd "     
 APP_SCHEME = 'https'
